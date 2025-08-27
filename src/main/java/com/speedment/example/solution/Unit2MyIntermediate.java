7dd128dd82e65d19a1e57c4f5f08b013440ce6e9
fallisBruno <bruno.falis@hotmail.com>
2025-08-27 15:05:34 -0300

    Solve Unit2

--- src/main/java/com/speedment/example/solution/Unit2MyIntermediate.java
+++ src/main/java/com/speedment/example/solution/Unit2MyIntermediate.java
@@ -9,31 +9,35 @@ public final class Unit2MyIntermediate implements Unit2Intermediate {
 
     @Override
     public Stream<String> wordsLongerThanThreeChars(Stream<String> stream) {
-        return Stream.empty();
+        return stream.filter(s -> s.length() > 3);
     }
 
     @Override
     public Stream<String> firstTwoWordsLongerThanThreeChars(Stream<String> stream) {
-        return Stream.empty();
+        return wordsLongerThanThreeChars(stream).limit(2);
     }
 
     @Override
     public Stream<String> firstDistinctTwoWordsLongerThanThreeCharsInAlphabeticOrder(Stream<String> stream) {
-        return Stream.empty();
+        return wordsLongerThanThreeChars(stream)
+                .distinct()
+                .limit(2)
+                .sorted();
     }
 
     @Override
     public IntStream lengthOfWords(Stream<String> stream) {
-        return IntStream.empty();
+        return stream.mapToInt(String::length);
     }
 
     @Override
     public IntStream increasingSawtooth() {
-        return IntStream.empty();
+        return IntStream.rangeClosed(0, Integer.MAX_VALUE)
+                .flatMap(i -> IntStream.rangeClosed(0, i));
     }
 
     @Override
     public Stream<String> strings(Stream<Object> stream) {
-        return Stream.empty();
+        return stream.filter(String.class::isInstance).map(Object::toString);
     }
 }
